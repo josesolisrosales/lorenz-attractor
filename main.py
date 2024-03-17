b917c87777a28ec7514778593e2837fd117d33e4
Jose Solis <josesolisrosales@gmail.com>
2024-03-17 14:47:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, TextBox
 
 # Define the Lorenz system
 def lorenz(x, y, z, s, r, b):
@@ -22,13 +21,17 @@ ax = fig.add_subplot(111, projection='3d')
 sigma = 10
 rho = 28
 beta = 8 / 3
+x_init, y_init, z_init = 0.0, 1.0, 1.05
 
 # Function to update the plot
 def update(val):
-    # Get the current values of the sliders
+    # Get the current values of the sliders and text boxes
     s = sigma_slider.val
     r = rho_slider.val
     b = beta_slider.val
+    x0 = float(x_text_box.text)
+    y0 = float(y_text_box.text)
+    z0 = float(z_text_box.text)
 
     # Initialize arrays to store the trajectory
     xs = np.empty(num_steps + 1)
@@ -36,7 +39,7 @@ def update(val):
     zs = np.empty(num_steps + 1)
 
     # Set initial values
-    xs[0], ys[0], zs[0] = (0., 1., 1.05)
+    xs[0], ys[0], zs[0] = x0, y0, z0
 
     # Generate the Lorenz fractal with the updated parameters
     for i in range(num_steps):
@@ -64,10 +67,23 @@ rho_slider = Slider(ax_rho, 'Rho', 0, 50, valinit=rho)
 ax_beta = plt.axes([0.2, 0.10, 0.65, 0.03])
 beta_slider = Slider(ax_beta, 'Beta', 0, 5, valinit=beta)
 
-# Call the update function when the slider value is changed
+# Create text boxes for setting initial values
+ax_x_init = plt.axes([0.2, 0.14, 0.15, 0.05])
+x_text_box = TextBox(ax_x_init, 'X0', initial=str(x_init))
+
+ax_y_init = plt.axes([0.45, 0.14, 0.15, 0.05])
+y_text_box = TextBox(ax_y_init, 'Y0', initial=str(y_init))
+
+ax_z_init = plt.axes([0.7, 0.14, 0.15, 0.05])
+z_text_box = TextBox(ax_z_init, 'Z0', initial=str(z_init))
+
+# Call the update function when the slider value is changed or text box is submitted
 sigma_slider.on_changed(update)
 rho_slider.on_changed(update)
 beta_slider.on_changed(update)
+x_text_box.on_submit(update)
+y_text_box.on_submit(update)
+z_text_box.on_submit(update)
 
 # Generate the initial plot
 update(None)
