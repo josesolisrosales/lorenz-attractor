f6b9723e129f2e01529f334b420959781473eb76
Jose Solis <jose.solisrosales@sysdig.com>
2026-06-27 14:47:09 -0600

    test: make realtime smoke tests import-order-robust; strengthen assertions

--- tests/test_visualization_realtime.py
+++ tests/test_visualization_realtime.py
@@ -12,9 +12,11 @@ from lorenz_attractor.core.simulator import Simulator
 def _mock_graphics_libs():
     mods = {
         name: mock.MagicMock()
-        for name in ("pygame", "OpenGL", "OpenGL.GL", "OpenGL.GLU", "moderngl")
+        for name in ('pygame', 'OpenGL', 'OpenGL.GL', 'OpenGL.GLU', 'moderngl')
     }
     with mock.patch.dict(sys.modules, mods):
+        for cached in ('lorenz_attractor.visualization.realtime',):
+            sys.modules.pop(cached, None)
         yield
 
 
@@ -22,11 +24,13 @@ def test_realtime_visualizer_constructs():
     from lorenz_attractor.visualization.realtime import RealtimeVisualizer
 
     viz = RealtimeVisualizer(Simulator(), trail_length=100)
-    assert viz is not None
+    assert isinstance(viz, RealtimeVisualizer)
+    assert viz.trail_length == 100
 
 
 def test_streaming_visualizer_constructs():
     from lorenz_attractor.visualization.realtime import StreamingVisualizer
 
     viz = StreamingVisualizer(Simulator(), buffer_size=500)
-    assert viz is not None
+    assert isinstance(viz, StreamingVisualizer)
+    assert viz.buffer_size == 500
